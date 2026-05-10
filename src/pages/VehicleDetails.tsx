90e162d398871e13a79309cc3baae6bd38e7d793
Casido1 <ugochukwu.anunihu@gmail.com>
2026-05-10 21:11:41 +0100

    feat: implement vehicle details page with real-time telemetry tracking and geofence management

--- src/pages/VehicleDetails.tsx
+++ src/pages/VehicleDetails.tsx
@@ -193,11 +193,11 @@ const VehicleDetails = () => {
                     zoom={15} 
                     scrollWheelZoom={false}
                     zoomControl={false}
-                    className="w-full h-full grayscale opacity-80"
+                    className="w-full h-full"
                 >
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
                     />
                     <Marker position={[lat, lng]} icon={carIcon} />
                     {geofences?.map((gf: Geofence) => (
